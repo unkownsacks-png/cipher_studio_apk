655d1a3ed3c3d22e01399c887a963299fe67ce17
CIPHER <birukgetachew253@gmail.com>
2026-01-25 17:56:54 +0300

    Update CodeLabScreen.kt

--- app/src/main/java/com/cipher/studio/presentation/codelab/CodeLabScreen.kt
+++ app/src/main/java/com/cipher/studio/presentation/codelab/CodeLabScreen.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
